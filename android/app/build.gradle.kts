cf30055ae1f143f9625fcd9dde52e30bc8a6e0ae
joseguerracajas <joseantonioguerracajas@gmail.com>
2026-04-28 17:40:50 -0500

    feat: agregar configuración de firma para la versión de lanzamiento y crear archivo de keystore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,21 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+signingConfigs {
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["keyPassword"] as String
+        storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+        storePassword = keystoreProperties["storePassword"] as String
+    }
+}
+
 android {
     namespace = "com.example.album_26_sticker_collector"
     compileSdk = flutter.compileSdkVersion
@@ -34,7 +49,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
